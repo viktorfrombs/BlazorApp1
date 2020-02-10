7d90ca8da661463b35ba39f55ad94b9c6dfa3690
Viktor Mazur <viktor.mazur@uptime.eu>
2020-02-10 17:46:38 +0200

    WorkAuth WITH Graph

--- BlazorApp1/obj/Debug/netcoreapp3.1/BlazorApp1.AssemblyInfo.cs
+++ BlazorApp1/obj/Debug/netcoreapp3.1/BlazorApp1.AssemblyInfo.cs
@@ -11,7 +11,7 @@
 using System;
 using System.Reflection;
 
-[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("aspnet-BlazorApp1-76A72EC5-8A41-414A-A07C-172DCA1D6498")]
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("aspnet-BlazorApp1-DE31C16C-65F8-4D90-913A-DE519C4F3652")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
